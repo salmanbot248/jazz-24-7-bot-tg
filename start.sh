c2df25a8ac65036758e357d4094c96d602a0a2f3
salmanbot248 <freefireidarif6@gmail.com>
2026-06-05 23:41:40 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+echo "Installing Python requirements..."
 pip install -r requirements.txt
+
+echo "Installing system tools..."
+apt-get install -y ffmpeg aria2 p7zip-full > /dev/null 2>&1
+
+echo "Installing Playwright..."
 playwright install chromium
 playwright install-deps chromium
+
+echo "Starting bot..."
 python bot.py
